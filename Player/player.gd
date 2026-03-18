092e570e5877c745d47694ef8e066f15cd42cd0d
mje819 <2260211@flourbluffschools.org>
2026-03-18 12:44:46 -0500

    fix player movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,19 +1,17 @@
 extends CharacterBody2D
 
 
-@export var speed := 150
+@export var speed := 300
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_down"):
-		velocity.y = speed
-	if Input.is_action_pressed("move_up"):
-		velocity.y = -speed
-		
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -speed
-	if Input.is_action_pressed("move_right"):
-		velocity.x = speed
-		
+
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
+
+
+func _physics_process(_delta: float) -> void:
+	
+	get_input()
 	
 	
 	move_and_slide()

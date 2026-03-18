f406f9086082a5dec0d1f833837c008249edb2c5
mje819 <2260211@flourbluffschools.org>
2026-03-18 12:59:28 -0500

    fixed scene change

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -5,4 +5,5 @@ extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("start battle")
-	#get_tree().change_scene_to_file("battle_screen.tscn")
+
+	get_tree().call_deferred("change_scene_to_file", "battle_screen.tscn")

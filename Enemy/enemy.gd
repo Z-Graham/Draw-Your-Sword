9ff11bfb288f0960118f52d90135a21339b820e5
mje819 <2260211@flourbluffschools.org>
2026-03-18 12:54:30 -0500

    add enemy

---
+++ Enemy/enemy.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	print("start battle")
+	#get_tree().change_scene_to_file("battle_screen.tscn")
--- Player/player.gd
+++ Player/player.gd
@@ -12,6 +12,4 @@ func get_input():
 func _physics_process(_delta: float) -> void:
 	
 	get_input()
-	
-	
 	move_and_slide()

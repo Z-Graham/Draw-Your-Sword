5602aa413e0a1e3ecb04b2d6126c41c77c66ea72
Z-Graham <2260220@flourbluffschools.org>
2026-03-24 20:24:51 -0500

    Fleshed out battle menu

--- battle_screen.gd
+++ battle_screen.gd
@@ -1,5 +1,12 @@
 extends Node2D
 @onready var inventory: Control = $Inventory
+@onready var main_battle_menu: Control = $Main_battle_menu
+@onready var fight_battle_menu: Control = $fight_battle_menu
+@onready var mp_bar: TextureProgressBar = $"MP bar"
+@onready var health_bar: TextureProgressBar = $"health bar"
+@onready var player_battle: AnimatedSprite2D = $player_battle
+@onready var enemy_battle: AnimatedSprite2D = $enemy_battle
+@onready var description: Label = $fight_battle_menu/Panel/Description
 
 var player_health=100
 var enemy_health=100
@@ -8,22 +15,59 @@ var handle="normal"
 var imbue="none"
 var draw_charge:int
 
+var in_main=true
+var in_battle=false
+var in_inventory=false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	draw_charge=5
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func player_fight(blade:String,handle:String,imbue:String):
-	print("fight")
+	pass
 
 
 func _on_fight_button_pressed() -> void:
-	player_fight(blade, handle, imbue)
-
+	main_battle_menu.visible=false
+	fight_battle_menu.visible=true
 
 func _on_item_button_pressed() -> void:
 	inventory.visible=true
+
+
+func _on_basic_button_pressed() -> void:
+	player_fight(blade,handle,imbue)
+
+func _on_basic_button_mouse_entered() -> void:
+	description.text="A basic attack with your weapon.
+	Restores 5MP"
+
+func _on_basic_button_mouse_exited() -> void:
+	description.text=""
+
+func _on_blade_skill_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_blade_skill_button_mouse_entered() -> void:
+	if blade=="normal":
+		description.text="Double Slice: Two rapid attacks with your weapon.
+		Costs 20MP"
+
+func _on_blade_skill_button_mouse_exited() -> void:
+	description.text=""
+
+func _on_handle_skill_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_handle_skill_button_mouse_entered() -> void:
+	if handle=="normal":
+		description.text="Bash: A strike with the pommel of your blade with 
+		a chance to confuse the enemy.
+		Costs 30MP"
+
+func _on_handle_skill_button_mouse_exited() -> void:
+	description.text=""
+
+func _on_back_button_pressed() -> void:
+	main_battle_menu.visible=true
+	fight_battle_menu.visible=false

0fa65d716c53a94b48f6ec8c4a9574df80d8bb8e
Z-Graham <2260220@flourbluffschools.org>
2026-03-24 20:37:40 -0500

    better organization

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -6,4 +6,4 @@ extends Area2D
 func _on_body_entered(_body: Node2D) -> void:
 	print("start battle")
 
-	get_tree().call_deferred("change_scene_to_file", "battle_screen.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://Other Menus/battle_screen.tscn")

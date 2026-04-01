1be5bb25afd9cce217507682a12c71b35e953d6a
Z-Graham <2260220@flourbluffschools.org>
2026-04-01 12:11:41 -0500

    fixed bug with zero of an item

--- Other Menus/inventory.gd
+++ Other Menus/inventory.gd
@@ -20,8 +20,9 @@ func _ready() -> void:
 	for item in (Globals.healing_items):
 		if Globals.healing_items[item]>1:
 			healing_item_list.add_item(item+"   x"+str(Globals.healing_items[item]))
-		else:
+		elif Globals.healing_items[item]==1:
 			healing_item_list.add_item(item)
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- globals.gd
+++ globals.gd
@@ -7,4 +7,4 @@ var player_current_MP=player_max_MP
 var player_stats={"max_health":100, "current_health":100, 
 "max_MP":50, "current_MP":50, "attack":10, "defense":10}
 
-var healing_items={"HP Potion":5, "MP Potion":12}
+var healing_items={"HP Potion":0, "MP Potion":0}

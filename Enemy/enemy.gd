532e2a4436906adbcca460bf486fbab6f297206e
mje819 <2260211@flourbluffschools.org>
2026-04-09 12:01:00 -0500

    allow battle screen to reset

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -6,6 +6,8 @@ extends Area2D
 func _on_body_entered(_body: Node2D) -> void:
 	print("start battle")
 
-	var battle_screen := get_tree().get_first_node_in_group("BattleScreenLayer")
-	battle_screen.visible = true
+	var battle_screen_layer := get_tree().get_first_node_in_group("BattleScreenLayer")
+	battle_screen_layer.visible = true
+	var battle_screen=get_tree().get_first_node_in_group("BattleScreenLayer").get_child(0)
+	battle_screen.reset()
 	queue_free()

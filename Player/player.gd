a85c86e3a64a04cd1deb4fdfb8e08398ca70911e
mje819 <2260211@flourbluffschools.org>
2026-04-14 12:37:12 -0500

    make ui labels match sword wip (use globals)

--- Player/player.gd
+++ Player/player.gd
@@ -7,6 +7,9 @@ var health_label
 var max_health : int  = Globals.player_stats["max_health"]
 var magic_label
 var max_magic : int = Globals.player_stats["max_MP"]
+var blade_label
+var handle_label
+var imbue_label
 
 
 #var health := max_health:
@@ -31,7 +34,10 @@ func _ready() -> void:
 	magic_label = get_tree().get_first_node_in_group("magiclabel")
 	Globals.player_stats["current_MP"] = max_magic
 	magic_label.text = "MP: " + str(Globals.player_stats["current_MP"]) +"/"+ str(max_magic)
-
+	
+	blade_label = get_tree().get_first_node_in_group("BladeLabel")
+	handle_label = get_tree().get_first_node_in_group("handleLabel")
+	imbue_label = get_tree().get_first_node_in_group("ImbueLabel")
 
 func get_input():
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -42,6 +48,9 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		health_label.text = "HP: " + str(Globals.player_stats["current_health"]) +"/"+ str(max_health)
 		magic_label.text = "MP: " + str(Globals.player_stats["current_MP"]) +"/"+ str(max_magic)
+		blade_label.text = "BLADE: " # + Globals
+		handle_label.text = "HANDLE: " # + Globals
+		imbue_label.text = "IMBUE: " # + Globals
 		
 		if !ui.visible:
 			ui.visible = true

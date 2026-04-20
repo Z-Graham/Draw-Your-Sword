cd5f68e2bc218582f7b9e82a6db0070be0442984
mje819 <2260211@flourbluffschools.org>
2026-04-20 12:27:56 -0500

    add hint labels to draw_item_screen

--- Other Menus/draw_item_screen.gd
+++ Other Menus/draw_item_screen.gd
@@ -9,9 +9,18 @@ var sel_object
 
 signal solution_gotten
 
+@onready var wrong_answer_label: Label = $WrongAnswerLabel
 
 func _on_object_list_item_selected(_index: int) -> void:
 	sel_object = object_list.get_item_text(_index)
+	
+
+
+func _on_confirm_button_pressed() -> void:
 	if sel_object == solution:
 		solution_gotten.emit(solution)
 		visible = false
+	else:
+		wrong_answer_label.visible = true
+		await get_tree().create_timer(1.5).timeout
+		wrong_answer_label.visible = false

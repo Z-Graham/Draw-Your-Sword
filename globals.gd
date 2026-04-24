0d037697be898b0338105057fc23fefc414e553e
Z-Graham <2260220@flourbluffschools.org>
2026-04-23 21:59:33 -0400

    final 4/23

--- globals.gd
+++ globals.gd
@@ -5,10 +5,10 @@ var player_max_MP=50
 var player_current_health=player_max_health
 var player_current_MP=player_max_MP
 var player_stats={"max_health":100, "current_health":100, 
-"max_MP":50, "current_MP":50, "attack":15, "defense":10, "Exp":0,
-}
+"max_MP":50, "current_MP":50, "attack":15, "defense":10, "Exp":0}
 
-
+var exp_requirements={"2":15,"3":20,"4":30,"5":30,"6":30,"7":40,"8":50,"9":50,"10":100}
+var level=1
 
 var healing_items={"HP Potion":0, "MP Potion":0}
 

069dd9ea345f8013a60af61ada55a1a9d5f06fe6
mje819 <2260211@flourbluffschools.org>
2026-04-30 12:41:26 -0500

    make key system wip (key group)

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @export_enum("goblin", "other") var type := "goblin"
+@export var has_key := false
 
+@onready var key: Sprite2D = $Key
+
+
+func _ready() -> void:
+	if has_key:
+		key.visible = true
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("start battle")
@@ -10,4 +17,6 @@ func _on_body_entered(_body: Node2D) -> void:
 	battle_screen_layer.visible = true
 	var battle_screen=get_tree().get_first_node_in_group("BattleScreenLayer").get_child(0)
 	battle_screen.reset()
+	if has_key:
+		Globals.key = true
 	queue_free()

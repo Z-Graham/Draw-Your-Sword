905529b61514d15e5510d5db6daa857ff5cee835
Z-Graham <2260220@flourbluffschools.org>
2026-05-20 13:06:57 -0500

    removed known parts

--- globals.gd
+++ globals.gd
@@ -22,9 +22,9 @@ var sword = {"blade":"basic","handle":"basic","imbue":"physical"}
 var tutorial_checks={"battle_screen":false,"fight":false,"draw_1":false,"inventory":false,"overworld draw":false}
 
 # Arrays of unlocked sword parts
-var known_blades = ["basic","katana","kris","spear","claymore"]
-var known_handles = ["basic","katana","kris","spear","claymore"]
-var known_imbues = ["physical","fire","lightning","wind","rock","ice"]
+var known_blades = ["basic"]
+var known_handles = ["basic"]
+var known_imbues = ["physical"]
 var known_draw_items = []
 
 var goblin_stats={"health":50,"defense":7,"attack":10,
